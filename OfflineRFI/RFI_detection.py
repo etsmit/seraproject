510e548c77c6972ddf2141c1d0f1da45048f5536
etsmit <etsmit12@gmail.com>
2020-09-30 15:48:18 -0400

    SIR function

--- OfflineRFI/RFI_detection.py
+++ OfflineRFI/RFI_detection.py
@@ -7,11 +7,9 @@ Supporting / misc functions in RFI_support
 
 1. Spectral Kurtosis
 
-
-
 2. Spectral Entropy/Relative Spectral Entropy
 
-
+3 . Scale-invariant Rank operator
 
 
 """
@@ -127,6 +125,86 @@ def entropy(a,nbit):
 		H[i] = np.sum(-num*np.log2(num))
 	return H
 
+
+
+#---------------------------------------------------------
+# 3 . Scale-invariant Rank operator - extends flags
+#---------------------------------------------------------
+
+
+
+
+#combine: 'union', 'vert', 'hori'
+def sir(a,row_eta,col_eta,combine):
+	"""
+	SIR operator - takes a input flagging mask and extend them in time/freq.
+	eta values determine how liberal extra flagging is. 1 = whole mask turns into flags, 0=no extra flags added.
+
+	Parameters
+	-----------
+	a : ndarray
+		2-dimensional array of mask bools assuming 1=flag,0=clean. Shape (Num Channels , Num Raw Spectra)
+	row_eta : float
+		float in range (0-1) to determine how many extra rows are flagged
+	col_eta : float
+		float in range (0-1) to determine how many extra columns are flagged
+	combine : str
+		must be chosen from ['union', 'vert', 'hori']. How column and row flags are combined.
+
+	Returns
+	-----------
+	out : ndarray
+		2-dimensional array of extended mask bools
+	"""
+
+	max_row_win = 1
+	max_col_win = 0.01
+    
+	sir_mask = np.array(a)
+	row_mask = np.zeros(a.shape)
+	col_mask = np.zeros(a.shape)
+
+	if combine == 'union':
+		print('doing rows...')
+		for row in range(a.shape[0]):
+			row_mask[row,:] = sir_1d(a[row,:],row_eta,max_row_win)
+		print('doing cols...')
+		for col in range(a.shape[1]):
+		col_mask[:,col] = sir_1d(a[:,col],col_eta,max_col_win)
+		sir_mask[row_mask==2]=2
+		sir_mask[col_mask==2]=2
+		#reset original flags back to 1's
+		#sir_mask[a==1]=1
+
+	if combine == 'vert':
+		for col in range(a.shape[1]):
+			col_mask[:,col] = sir_1d(a[:,col],col_eta,max_col_win)
+		col_mask[col_mask==2]=1
+		for row in range(a.shape[0]):
+			row_mask[row,:] = sir_1d(col_mask[row,:],row_eta,max_row_win)
+		sir_mask[row_mask==2]=3
+		sir_mask[col_mask==2]=2
+		#reset original flags back to 1's
+		#sir_mask[a==1]=1
+        
+	if combine == 'hori':
+		for row in range(a.shape[0]):
+			row_mask[row,:] = sir_1d(a[row,:],row_eta,max_row_win)
+		row_mask[row_mask==2]=1
+		for col in range(a.shape[1]):
+			col_mask[:,col] = sir_1d(row_mask[:,col],col_eta,max_col_win)
+		sir_mask[col_mask==2]=3
+		sir_mask[row_mask==2]=2
+		#reset original flags back to 1's
+		#sir_mask[a==1]=1
+
+	#set all flags to 1
+	#(this isn't done in the meat of the function in the case you want to compare original to extended)
+	sir_mask[sir_mask != 0] = 1
+
+	
+        
+    return sir_mask
 
 
 
